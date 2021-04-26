ce7d12d31c32a1700ab476638d8e8a059531cd9c
Lisa PR <l.prat.rivero@gmail.com>
2021-04-26 17:48:27 +0200

    chapter 5 ex. 3 done

--- 05-arrays/03-walk-one/script.js
+++ 05-arrays/03-walk-one/script.js
@@ -9,9 +9,8 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
-    var fruits = [
+(function () {
+    let fruits = [
         "apple",
         "pear",
         "raspberry",
@@ -26,6 +25,16 @@
         "cherry",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function () {
+        for (i = 0; i < fruits.length; i++) {
+            console.log(fruits[i]);
+        }
+    });
 
 })();
+
+// click the button
+// show value one after the other (loop)
+// First we set a variable before the loop start : i=0
+// Second we set a condition for the loop to run: as long as the value is lower than the length, it wil run
+// Third we add value to the initial value: to display the next element in the loop
