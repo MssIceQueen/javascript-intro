c9b934c1250681f84070c6275024fc91f9240795
Lisa PR <l.prat.rivero@gmail.com>
2021-04-26 12:40:27 +0200

    fixed issue with fizzbuzz

--- 04-maths/03-fizzbuzz/script.js
+++ 04-maths/03-fizzbuzz/script.js
@@ -12,14 +12,14 @@
 (function() {
         let  a;
         for (a = 1; a <= 100; a++) {
-        if (a % 3 === 0) {
-            console.log("fizz");
+        if (a % 3 === 0 && a % 5 === 0) {
+            console.log("fizzbuzz");
         }
         else if (a % 5 === 0) {
             console.log("buzz");
         }
-        else if (a % 3 === 0 && a % 5 === 0) {
-            console.log("fizzbuzz");
+        else if (a % 3 === 0) {
+            console.log("fizz") ;
         }
         else {
             console.log(a);
